6d87dcadd074236ef3401b38fb5df9a14e3ff555
Alex Cho <alexsjcho@gmail.com>
2019-02-25 19:49:24 -0800

    worked through RouletteGun assignment drill

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./App.css";
 // import Bomb from "./state-drills/Bomb";
-import HelloWorld from "./state-drills/HelloWorld";
-
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+// import HelloWorld from "./state-drills/HelloWorld";
+import RouletteGun from "./state-drills/RouletteGun";
+ReactDOM.render(<RouletteGun />, document.getElementById("root"));
