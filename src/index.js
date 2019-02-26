3dcd64387a6c259807431eb64f02529eebb2f4fa
Alex Cho <alexsjcho@gmail.com>
2019-02-25 19:40:09 -0800

    worked on Bomb.js assignment

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./App.css";
+// import Bomb from "./state-drills/Bomb";
+import HelloWorld from "./state-drills/HelloWorld";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
