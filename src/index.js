419b9be94bfdbf7488ddf04f86c313abb1e749b4
Alex Cho <alexsjcho@gmail.com>
2019-02-26 12:30:24 -0800

    worked on Accordian drill

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import App from "./App";
 import "./App.css";
 // import Bomb from "./state-drills/Bomb";
 // import HelloWorld from "./state-drills/HelloWorld";
-import RouletteGun from "./state-drills/RouletteGun";
-ReactDOM.render(<RouletteGun />, document.getElementById("root"));
+// import RouletteGun from "./state-drills/RouletteGun";
+import Accordian from "./state-drills/Accordian";
+ReactDOM.render(<Accordian />, document.getElementById("root"));
